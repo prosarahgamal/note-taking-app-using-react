6095cca5bef0f9641cf67ac4f2c001d08f397b60
Sara Eisa <prosarahgamal@outlook.com>
2019-02-22 00:22:19 +0200

    board-component-added

--- src/App.js
+++ src/App.js
@@ -1,7 +1,62 @@
 import React from 'react';
-
+import Note from  './note';
 import './App.css';
 
+var Board = React.createClass({
+  propTypes: {
+    count: function (props, propName) {
+      if (typeof props[propName] !== 'number') {
+        return new Error('the count should be a number')
+      }
+      if (props[propName] > 100) {
+        return new Error('no more than 100')
+      }
+    }
+  },
+  getInitialState(){
+    return {notes: []}
+  },
+  generateId(){
+    this.unique = this.unique || 0
+    return this.unique++
+  },
+  add(text){
+    var notes = [
+      ...this.state.notes,
+      {
+        id: this.generateId(),
+        text
+      }
+    ]
+    this.setState({notes});
+  },
+  update(newTxt, id){
+    var notes = this.state.notes.map( note => 
+      (note.id !== id) ? note :{ id: id, text: newTxt }
+    )
+    this.setState({notes})
+  },
+  remove(id){
+    var notes = this.state.notes.filter((note) => note.id !== id)
+    this.setState({notes})
+  },
+  eachNote(note){
+    console.log(note);
+    return ( <Note key={note.id}
+                   id = {note.id}
+                   onClick = {this.update}
+                   onRemove = {this.remove}>
+                    {note.text}
+                    </Note> )
+  },
+  render(){
+    return (
+      <div className="board">
+        {this.state.notes.map(this.eachNote)}
+        <button onClick={() => this.add('enter note...')}>+</button>
+      </div>
+    ) 
+  }
+})
 
-
-export default App;
+export default Board
